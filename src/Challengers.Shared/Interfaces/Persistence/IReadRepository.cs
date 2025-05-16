1080b3640e770e3cd2feb740d401270516264e96
Simon Holmquist <simonholmquist@outlook.com>
2025-05-16 17:25:59 -0300

    chore(shared): add shared project and generic repository interfaces

---
+++ src/Challengers.Shared/Interfaces/Persistence/IReadRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Challengers.Shared.Interfaces.Persistence;
+
+public interface IReadRepository<T> where T : class
+{
+    Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<List<T>> GetAllAsync(CancellationToken cancellationToken = default);
+}
---
+++ src/Challengers.Shared/Interfaces/Persistence/IWriteRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Challengers.Shared.Interfaces.Persistence;
+
+public interface IWriteRepository<T> where T : class
+{
+    Task AddAsync(T entity, CancellationToken cancellationToken = default);
+    void Update(T entity);
+    void Delete(T entity);
+    Task SaveChangesAsync(CancellationToken cancellationToken = default);
+}
