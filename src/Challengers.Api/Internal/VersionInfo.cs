17fc5b657666c99ad61b3d1ef81942e7060938ed
Simon Holmquist <simonholmquist@outlook.com>
2025-05-19 01:30:55 -0300

    feat(api): add /version endpoint exposing app version and commit SHA

---
+++ src/Challengers.Api/Internal/VersionInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Challengers.Api.Internal;
+
+public static class VersionInfo
+{
+    public static string Version => typeof(Program).Assembly
+        .GetName().Version?.ToString() ?? "unknown";
+
+    public static string CommitSha => Environment.GetEnvironmentVariable("GIT_COMMIT_SHA") ?? "unknown";
+}
